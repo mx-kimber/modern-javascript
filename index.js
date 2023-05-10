959a31e07f63c3d491dfd069ad4fafa44687f848
Kimber Ogden <mx.kimber@gmail.com>
2023-05-10 12:29:09 -0500

    learning js libraries

---
+++ index.js
@@ -0,0 +1,9 @@
+var moment = require('moment');
+console.log(moment().format());
+
+console.log('hello!');
+
+// console.log(moment().format('dddd'));
+// console.log(moment().format("MMM Do YY"));
+
+
