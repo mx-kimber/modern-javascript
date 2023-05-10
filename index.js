cf0fd73921eb6a469f07448fe4eabcb1557351e3
Kimber Ogden <mx.kimber@gmail.com>
2023-05-10 13:37:04 -0500

    added mathjs const

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log(moment().format('dddd'));
 console.log(moment().format("MMM Do YY"));
 
 const { sqrt } = require('mathjs');
+console.log(sqrt(-4).toString());
 
-console.log(sqrt(-4).toString()); // 2i
-
-
+const { simplify, parse, derivative } = require('mathjs');
+console.log(simplify('2x + 3x').toString());
