30ef62a29e151c69f621bb3e615e5ae807d724c5
Kimber Ogden <mx.kimber@gmail.com>
2023-05-10 13:27:37 -0500

    added webpack.config

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var moment = require('moment');
-console.log(moment().format());
+/* global moment */
+var moment = require('moment'); // require
+moment().format();
 
 console.log('hello!');
 
-// console.log(moment().format('dddd'));
-// console.log(moment().format("MMM Do YY"));
+console.log(moment().format('dddd'));
+console.log(moment().format("MMM Do YY"));
+
+const { sqrt } = require('mathjs');
+
+console.log(sqrt(-4).toString()); // 2i
 
 
